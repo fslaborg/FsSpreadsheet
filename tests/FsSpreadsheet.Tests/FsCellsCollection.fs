e5b112c876660c525a9e7b22eef2667272763b7b
omaus <oliver.maus@eclipso.de>
2023-03-19 15:50:04 +0100

    Add FsCellsCollection tests (WIP)

---
+++ tests/FsSpreadsheet.Tests/FsCellsCollection.fs
@@ -0,0 +1,34 @@
+﻿module FsCellsCollection
+
+open Expecto
+open FsSpreadsheet
+
+
+let dummyFsCells =
+    [
+        [
+            FsCell.createWithDataType DataType.String 1 1 "A1"
+            FsCell.createWithDataType DataType.String 1 2 "B1"
+            FsCell.createWithDataType DataType.String 1 3 "C1"
+        ]
+        [
+            FsCell.createWithDataType DataType.String 2 1 "A2"
+            FsCell.createWithDataType DataType.String 2 2 "B2"
+            FsCell.createWithDataType DataType.String 2 3 "C2"
+        ]
+        [
+            FsCell.createWithDataType DataType.String 3 1 "A3"
+            FsCell.createWithDataType DataType.String 3 2 "B3"
+            FsCell.createWithDataType DataType.String 3 3 "C3"
+        ]
+    ]
+let dummyFsCellsCollection = FsCellsCollection()
+dummyFsCells |> Seq.iter (dummyFsCellsCollection.Add >> ignore)
+
+
+
+[<Tests>]
+let fsCellsCollectionTests =
+    testList "FsCellsCollection" [
+        
+    ]
