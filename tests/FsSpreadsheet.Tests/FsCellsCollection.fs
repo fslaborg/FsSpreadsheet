f90b0dc7fe0ce0731dace8637fd4aab68b37f84a
omaus <oliver.maus@eclipso.de>
2023-03-19 16:54:25 +0100

    Finalize current FsCellsCollection tests
    I.e. `Add` (overload `seq<FsCell>`), `GetCells`, `GetFirstAddress`, and `GetLastAddress`.

--- tests/FsSpreadsheet.Tests/FsCellsCollection.fs
+++ tests/FsSpreadsheet.Tests/FsCellsCollection.fs
@@ -22,6 +22,7 @@ let dummyFsCells =
             FsCell.createWithDataType DataType.String 3 3 "C3"
         ]
     ]
+    |> Seq.concat
 let dummyFsCellsCollection = FsCellsCollection()
 dummyFsCells |> Seq.iter (dummyFsCellsCollection.Add >> ignore)
 
@@ -30,5 +31,21 @@ dummyFsCells |> Seq.iter (dummyFsCellsCollection.Add >> ignore)
 [<Tests>]
 let fsCellsCollectionTests =
     testList "FsCellsCollection" [
-        
+        testList "Add | GetCells" [
+            testCase "FsCells are present" <| fun _ ->
+                let gottenCells = dummyFsCellsCollection.GetCells()
+                Expect.containsAll gottenCells dummyFsCells "Cells were not added properly"
+        ]
+        testList "GetFirstAddress" [
+            testCase "First FsAddress is correct" <| fun _ ->
+                let gottenFirstAddress = dummyFsCellsCollection.GetFirstAddress() |> fun a -> a.RowNumber, a.ColumnNumber
+                let expectedFirstAddress = FsAddress(1, 1) |> fun a -> a.RowNumber, a.ColumnNumber
+                Expect.equal gottenFirstAddress expectedFirstAddress "First addresses are not equal"
+        ]
+        testList "GetLastAddress" [
+            testCase "Last FsAddress is correct" <| fun _ ->
+                let gottenFirstAddress= dummyFsCellsCollection.GetLastAddress() |> fun a -> a.RowNumber, a.ColumnNumber
+                let expectedFirstAddress = FsAddress(3, 3) |> fun a -> a.RowNumber, a.ColumnNumber
+                Expect.equal gottenFirstAddress expectedFirstAddress "Last addresses are not equal"
+        ]
     ]
