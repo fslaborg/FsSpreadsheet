c4291fba92d7e01c0feb3e2f2a1b53d266f0c7a3
omaus <omaus@bio.uni-kl.de>
2023-03-20 18:03:43 +0100

    Add table field unit tests (WIP)

---
+++ tests/FsSpreadsheet.Tests/FsTableField.fs
@@ -0,0 +1,26 @@
+﻿module FsTableField
+
+open FsSpreadsheet
+open Expecto
+
+
+let dummyFsRangeAddress = FsRangeAddress("A1:A3")
+let dummyFsRangeColumn = FsRangeColumn(dummyFsRangeAddress)
+let dummyFsTableField = FsTableField("dummyFsTableField", 0, dummyFsRangeColumn, obj, obj)
+let dummyFsCells = [
+    FsCell.createWithDataType DataType.String 1 1 "I am the Header!"
+    FsCell.createWithDataType DataType.String 2 1 "first data cell"
+    FsCell.createWithDataType DataType.String 3 1 "second data cell"
+]
+let dummyFsCellsCollection = FsCellsCollection()
+dummyFsCellsCollection.Add dummyFsCells |> ignore
+
+
+[<Tests>]
+let fsTableField =
+    testList "FsTableField" [
+        testList "" [
+            testCase "" <| fun _ ->
+                Expect.equal  "not equal"
+        ]
+    ]
