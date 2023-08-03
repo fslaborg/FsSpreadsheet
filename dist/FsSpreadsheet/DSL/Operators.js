77790114c7d652e818ab1c9492f730ce573c47b3
Kevin F <Freymaurer@gmx.de>
2023-08-03 13:53:52 +0200

    delete dist

--- dist/FsSpreadsheet/DSL/Operators.js
+++
@@ -1,6 +0,0 @@
-import { RequiredSource$1_$ctor_2B595, OptionalSource$1_$ctor_2B595 } from "./Expression.js";
-
-export const optional = OptionalSource$1_$ctor_2B595(void 0);
-
-export const required = RequiredSource$1_$ctor_2B595(void 0);
-
