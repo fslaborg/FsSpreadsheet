886dcf2ef7459096bf1a35a4fc6173e33f424758
Heinrich Lukas Weil <hl.weil@t-online.de>
2024-02-02 11:17:51 +0100

    fix zipArchiveReader against default tests

--- src/FsSpreadsheet.ExcelIO/ZipArchiveReader.fs
+++ src/FsSpreadsheet.ExcelIO/ZipArchiveReader.fs
@@ -1,6 +1,5 @@
 ﻿module FsSpreadsheet.ExcelIO.ZipArchiveReader
 
-
 open System.IO.Compression
 open System.Xml
 
@@ -27,7 +26,7 @@ module DType =
     [<Literal>]
     let sharedString = "s"
     [<Literal>]
-    let string = "4"
+    let string = "str"
     [<Literal>]
     let inlineString = "5"
     [<Literal>]
@@ -87,18 +86,23 @@ and Styles =
     }
 
 
+module XmlReader =
+    let isElemWithName (reader : System.Xml.XmlReader) (name : string) =
+        reader.NodeType = XmlNodeType.Element && (reader.Name = name || reader.Name = "x:" + name)
+
 let parseRelationsships (relationships : ZipArchiveEntry) : Relationships =
     try 
         let relationshipsStream = relationships.Open()
         let relationshipsReader = System.Xml.XmlReader.Create(relationshipsStream)
         let mutable rels = [||]
         while relationshipsReader.Read() do
-            if relationshipsReader.NodeType = XmlNodeType.Element && relationshipsReader.Name = "Relationship" then
+            if XmlReader.isElemWithName relationshipsReader "Relationship" then
                 let id = relationshipsReader.GetAttribute("Id")
                 let typ = relationshipsReader.GetAttribute("Type")
                 let target = 
                     let t = relationshipsReader.GetAttribute("Target")
                     if t.StartsWith "xl/" then t
+                    elif t.StartsWith "/xl/" then t.Replace("/xl/","xl/")
                     elif t.StartsWith "../" then t.Replace("../","xl/")
                     else "xl/" + t
                 rels <- Array.append rels [|{Id = id; Type = typ; Target = target}|]
@@ -123,9 +127,11 @@ let getSharedStrings (wb : WorkBook) : SharedStrings =
         let sharedStringsReader = System.Xml.XmlReader.Create(sharedStringsStream)
         [|
         while sharedStringsReader.Read() do
-            if sharedStringsReader.NodeType = System.Xml.XmlNodeType.Element && sharedStringsReader.Name = "si" then
-                sharedStringsReader.ReadToDescendant("t") |> ignore
-                yield sharedStringsReader.ReadElementContentAsString()
+            if XmlReader.isElemWithName sharedStringsReader "si" then
+                let subReader = sharedStringsReader.ReadSubtree()
+                while subReader.Read() do
+                    if XmlReader.isElemWithName subReader "t" then
+                        yield subReader.ReadElementContentAsString()
         |]
     with
     | _ -> [||]
@@ -137,23 +143,23 @@ let getStyles (wb : WorkBook) =
     let mutable numberFormats = Map.empty
     let mutable cellFormats = Array.empty
     while stylesReader.Read() do
-        if stylesReader.NodeType = XmlNodeType.Element && stylesReader.Name = "numFmts" then
+        if XmlReader.isElemWithName stylesReader "numFmts" then
             let subReader = stylesReader.ReadSubtree()
             let numFmts = 
                 [|
                     while subReader.Read() do
-                        if subReader.NodeType = XmlNodeType.Element && subReader.Name = "numFmt" then
+                        if XmlReader.isElemWithName subReader "numFmt" then
                             let id = subReader.GetAttribute("numFmtId") |> int                    
                             let formatCode = subReader.GetAttribute("formatCode")
                             yield id, {Id = id; FormatCode = formatCode}
                 |]
             numberFormats <- Map.ofArray numFmts
-        if stylesReader.NodeType = XmlNodeType.Element && stylesReader.Name = "cellXfs" then
+        if XmlReader.isElemWithName stylesReader "cellXfs" then
             let subReader = stylesReader.ReadSubtree()
             let cellFmts = 
                 [|
                     while subReader.Read() do
-                        if subReader.NodeType = XmlNodeType.Element && subReader.Name = "xf" then
+                        if XmlReader.isElemWithName subReader "xf" then
                             let numFmtId = subReader.GetAttribute("numFmtId") |> int
                             let applyNumberFormat = subReader.GetAttribute("applyNumberFormat") |> bool
                             yield {NumberFormatId = numFmtId; ApllyNumberFormat = applyNumberFormat}
@@ -166,23 +172,28 @@ let parseTable (sheet : ZipArchiveEntry) =
     try
         let stream = sheet.Open()
         let reader = System.Xml.XmlReader.Create(stream)
-        while reader.Name <> "table" do
-            reader.Read() |> ignore 
-        let area = reader.GetAttribute("ref")
-        let ra = FsRangeAddress(area)    
-        let totalsRowShown = 
-            let attribute = reader.GetAttribute("totalsRowShown")
-            match attribute with
-            | null 
-            | "0" -> false
-            | "1" -> true
-            | _ -> false
-        let name =
-            let dn = reader.GetAttribute("displayName")
-            if dn = null then 
-               reader.GetAttribute("name")
-            else dn
-        FsTable(name, ra, totalsRowShown, true)
+        let mutable t = None
+        while reader.Read() do
+            if XmlReader.isElemWithName reader "table" then
+                let area = reader.GetAttribute("ref")
+                let ra = FsRangeAddress(area)    
+                let totalsRowShown = 
+                    let attribute = reader.GetAttribute("totalsRowShown")
+                    match attribute with
+                    | null 
+                    | "0" -> false
+                    | "1" -> true
+                    | _ -> false
+                let name =
+                    let dn = reader.GetAttribute("displayName")
+                    if dn = null then 
+                       reader.GetAttribute("name")
+                    else dn
+                t <- Some (FsTable(name, ra, totalsRowShown, true))
+        if t.IsNone then
+            failwith "No table found"
+        else
+            t.Value
     with
     | err -> failwithf "Error while parsing table \"%s\":%s" sheet.FullName err.Message
 
@@ -200,65 +211,54 @@ let getTables (wb : WorkBook) =
         else None)
     |> Seq.toArray
 
-let parseDataType (styles : Styles) (dataType : string) (style : string) (formula : string) = 
-    if formula <> null then
-        // LibreOffice annotates boolean values as formulas instead of boolean datatypes
-        if formula = "TRUE()" || formula = "FALSE()" then
-            DataType.Boolean
-        else
-            DataType.Number
-    //https://stackoverflow.com/a/13178043/12858021
-    //https://stackoverflow.com/a/55425719/12858021
-    // if styleindex is not null and datatype is null we propably have a DateTime field.
-    // if datatype would not be null it could also be boolean, as far as i tested it ~Kevin F 13.10.2023
-    elif style <> null && (dataType = null || dataType = DType.number) then
-        try
-            let cellFormat : CellFormat = styles.CellFormats.[int style]
-            if (*cellFormat <> null &&*) CellFormat.isDateTime styles cellFormat then
-                DataType.Date
-                        
-            else
-                DataType.Number
-        with
-        | _ -> DataType.Number
-    else 
-        match dataType with
-        | DType.number -> DataType.Number
-        | DType.boolean -> DataType.Boolean
-        | DType.date -> DataType.Date
-        | DType.error -> DataType.Empty
-        | DType.inlineString
-        | DType.sharedString
-        | DType.string -> DataType.String
-        | _ -> DataType.Number
-
-let parseCell (sst : SharedStrings) (styles : Styles) (value : string) (dataType : string) (style : string) (formula : string) : obj*DataType =
-
-    let cellValueString = if dataType <> null && dataType = DType.sharedString then sst.[int value] else value
-    let dt = parseDataType styles dataType style formula
-    match dt with
-    | Date ->
-        try 
-            // datetime is written as float counting days since 1900. 
-            // We use the .NET helper because we really do not want to deal with datetime issues.
-            System.DateTime.FromOADate(float cellValueString)
-        with 
-        | _ -> cellValueString
-    | Boolean ->
-        // boolean is written as int/float either 0 or null
-        match cellValueString.ToLower() with 
-        | "1" | "true" -> true 
-        | "0" | "false" -> false 
-        | _ -> cellValueString
-    | Number -> 
-        try 
-            float cellValueString
-        with 
+let parseCell (sst : SharedStrings) (styles : Styles) (value : string) (dataType : string) (style : string) (formula : string) : obj*DataType = 
+    // LibreOffice annotates boolean values as formulas instead of boolean datatypes
+    if formula <> null && formula = "TRUE()"  then
+        true,DataType.Boolean
+    elif formula <> null && formula = "FALSE()" then
+        false,DataType.Boolean
+    else
+        let cellValueString = if dataType <> null && dataType = DType.sharedString then sst.[int value] else value
+        //https://stackoverflow.com/a/13178043/12858021
+        //https://stackoverflow.com/a/55425719/12858021
+        // if styleindex is not null and datatype is null we propably have a DateTime field.
+        // if datatype would not be null it could also be boolean, as far as i tested it ~Kevin F 13.10.2023
+        if style <> null && (dataType = null || dataType = DType.number) then
+            try
+                let cellFormat : CellFormat = styles.CellFormats.[int style]
+                if (*cellFormat <> null &&*) CellFormat.isDateTime styles cellFormat then
+                    System.DateTime.FromOADate(float cellValueString), DataType.Date                      
+                else
+                    float value, DataType.Number
+            with
+            | _ -> value, DataType.Number
+        else 
+            match dataType with
+            | DType.boolean -> 
+                match cellValueString.ToLower() with 
+                | "1" | "true" -> true 
+                | "0" | "false" -> false 
+                | _ -> cellValueString
+                ,
+                DataType.Boolean
+            | DType.date -> 
+                try 
+                    // datetime is written as float counting days since 1900. 
+                    // We use the .NET helper because we really do not want to deal with datetime issues.
+                    System.DateTime.FromOADate(float cellValueString), DataType.Date
+                with 
+                | _ -> cellValueString, DataType.Date
+            | DType.error -> cellValueString, DataType.Empty
+            | DType.inlineString
+            | DType.sharedString
+            | DType.string -> cellValueString, DataType.String
             | _ -> 
-                cellValueString
-    | Empty | String -> cellValueString
-    ,
-    dt
+                try 
+                    float cellValueString
+                with 
+                | _ -> cellValueString
+                ,
+                DataType.Number
 
 let parseWorksheet (name : string) (styles : Styles) (sharedStrings : SharedStrings) (sheet : ZipArchiveEntry) (wb : WorkBook) =
     try 
@@ -267,7 +267,7 @@ let parseWorksheet (name : string) (styles : Styles) (sharedStrings : SharedStri
         let reader = System.Xml.XmlReader.Create(stream)
         let relationships = getWsRelationships sheet.FullName wb
         while reader.Read() do
-            if reader.NodeType = XmlNodeType.Element && reader.Name = "c" then
+            if XmlReader.isElemWithName reader "c" then
                 let r = reader.GetAttribute("r")
                 let t = reader.GetAttribute("t")
                 let s = reader.GetAttribute("s")
@@ -276,24 +276,20 @@ let parseWorksheet (name : string) (styles : Styles) (sharedStrings : SharedStri
                 let mutable f = null
                 let cellReader = reader.ReadSubtree()
                 while cellReader.Read() do
-                    if cellReader.NodeType = XmlNodeType.Element && cellReader.Name = "v" then
+                    if XmlReader.isElemWithName cellReader "v" then
                         v <- cellReader.ReadElementContentAsString()
-                    if cellReader.NodeType = XmlNodeType.Element && cellReader.Name = "f" then
+                    if XmlReader.isElemWithName cellReader "f" then
                         f <- cellReader.ReadElementContentAsString()
-                if v <> null && v <> "" then
+                if v <> null && v <> "" || f <> null then
                     let cellValue,dataType = parseCell sharedStrings styles v t s f
                     let cell = FsCell(cellValue,dataType = dataType,address = FsAddress(r))
                     ws.AddCell(cell) |> ignore
-            if reader.NodeType = XmlNodeType.Element && reader.Name = "tablePart" then
-                printfn "TablePart"
+            if XmlReader.isElemWithName reader "tablePart" then
                 let id = reader.GetAttribute("r:id")
-                printfn "ID: %s" id
                 let rel = relationships.[id]
-                printf "Target: %s" rel.Target
                 let table = 
                     wb.GetEntry(rel.Target)
                     |> parseTable
-                printfn "tableref: %s" table.RangeAddress.Range
                 ws.AddTable(table) |> ignore
         reader.Close()
         ws
@@ -310,7 +306,7 @@ let parseWorkbook (wb : ZipArchive) =
     let wbStream = wbPart.Open()
     let wbReader = System.Xml.XmlReader.Create(wbStream)
     while wbReader.Read() do
-        if wbReader.Name = "sheet" then
+        if XmlReader.isElemWithName wbReader "sheet" then
             let name = wbReader.GetAttribute("name")
             let id = wbReader.GetAttribute("r:id")
             let rel = relationships.[id]
@@ -319,6 +315,7 @@ let parseWorkbook (wb : ZipArchive) =
             ws.RescanRows()
             newWb.AddWorksheet (ws) |> ignore
     newWb
+
 
 module FsWorkbook = 
     
